94b60baf8e3e12921b172db828a7f2774861255f
alex <alex.corderoluna@outlook.es>
2023-04-26 18:02:45 -0500

    Transformacionde datos y tipo booleanos

---
+++ 04_data_types/booleans.py
@@ -0,0 +1,8 @@
+is_true=True
+print(type(is_true))
+
+is_false=False
+print(type(is_false))
+
+print(not is_true)
+print(not is_false)
---
+++ 04_data_types/transform_data.py
@@ -0,0 +1,19 @@
+name="Aelxis"
+print(type(name))
+name=12
+print(type(name))
+name=True
+print(type(name))
+
+print("Alexis"+"Cordeo")
+print(10+10)
+
+age=26
+print("Mi edad es " + str(age))
+print(f"Mi edad es {age}")
+
+age = int(input("Ingresa tu edad => "))
+print(age)
+
+age+=10
+print(f"Tu edad en 10 años sera {age}")
