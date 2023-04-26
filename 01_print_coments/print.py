304a595c5e55e7b4a243942462e3e07aaa9619ea
alex <alex.corderoluna@outlook.es>
2023-04-25 22:11:12 -0500

    Organizacion de lecciones de curso

---
+++ 01_print_coments/print.py
@@ -0,0 +1,13 @@
+print("Hola, esto es python y esta es la funcion print")
+print("hola soy Alexis y tengo 26 años")
+
+print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+"""Estos son comentarios con 
+dobles commillar"""
+
+'''Estos son comentarios 
+con comillas simples'''
---
+++ 02_vars/vars.py
@@ -0,0 +1,12 @@
+print("Hola, vars")
+
+# esto es una variable
+my_name="Alexis"
+print(my_name)
+
+# esto es variable con numero
+my_age=26
+print(my_age)
+
+# concatenar salida
+print(my_name,"tiene",my_age)
