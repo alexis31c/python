8eed1acf00ecc2db52c2dd3a01309978fdbd8758
alex <alex.corderoluna@outlook.es>
2023-04-25 23:35:21 -0500

    tipos de datos, string

---
+++ 04_data_types/data_types.py
@@ -0,0 +1,20 @@
+# string
+my_name ="Alexis"
+my_secondName ="Arturo"
+print(my_name,my_secondName)
+print(type(my_name),type(my_secondName))
+
+#int
+my_age=26
+print(my_age)
+print(type(my_age))
+
+#boolean
+is_single=True
+print(is_single)
+print(type(is_single))
+
+#inputs
+my_age=input("Cual es tu edad? ")
+print(my_age)
+print(type(my_age))
