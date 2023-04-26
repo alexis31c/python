d4844bfbabf84f78b5d87bd881ea36194a932b09
alex <alex.corderoluna@outlook.es>
2023-04-25 21:57:35 -0500

    print y comentarios en python

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("Hola, esto es pytho y voy a aprender este lenguaje")
+print("hola soy Alexis y tengo 26 años")
+
+print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+"""Estos son comentarios con 
+dobles commillar"""
+
+'''Estos son comentarios 
+con comillas simples'''
