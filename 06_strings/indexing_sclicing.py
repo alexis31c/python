1ae518499015f981dbd74a1e60a8fce5abe9a5d5
alex <alex.corderoluna@outlook.es>
2023-04-28 19:26:10 -0500

    indexing y sclicing

---
+++ 06_strings/indexing_sclicing.py
@@ -0,0 +1,19 @@
+text="Hola mundo"
+print(text[0])
+print(text[1])
+
+size = len(text)
+print("size =>",size)
+print(text[size-1])
+print(text[-1])
+
+# slicing
+
+print(text[0:4])
+print(text[5:10])
+print(text[:10])
+print(text[5:])
+print(text[5:-1])
+print(text[5:10:2])
+print(text[5:10:1])
+print(text[::2])
