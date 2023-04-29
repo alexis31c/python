3822011b154c5d0aec827e9672552feec33ea197
alex <alex.corderoluna@outlook.es>
2023-04-28 19:50:58 -0500

    Listas en python

---
+++ 07_lists/list.py
@@ -0,0 +1,14 @@
+numbers=[1,2,3,4]
+print(numbers)
+print(numbers[1])
+print(numbers[:3])
+print(type(numbers))
+
+pr_languages=["C#","Python","JavaScript"]
+print(pr_languages)
+pr_languages[2]="Go"
+print(pr_languages)
+
+types=[1,True,"Hola mundo"]
+print(True in types)
+print(types)
