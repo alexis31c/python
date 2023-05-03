387fb125f8fde7956a9d813436014f7ea8b85339
alex <alex.corderoluna@outlook.es>
2023-05-03 11:55:29 -0500

    dictrionary in python

--- Fundamentals/09_dictionaries/dictionary.py
+++ Fundamentals/09_dictionaries/dictionary.py
@@ -17,3 +17,30 @@ print(my_dict.get('age'))
 
 print('avion' in my_dict)
 print('otroqueno' in my_dict)
+
+#Insert, Update and Delete
+
+person={
+    'name':'Alexis',
+    'last_name':'Cordero',
+    'langs':['C#','C++','Python','JavaScript'],
+    'age':26
+}
+print(person)
+
+person['name']='Alexis Cordero'
+person['langs'].append("Go")
+print(person)
+
+del person['last_name']
+person.pop('age')
+print(person)
+
+print('items')
+print(person.items())
+
+print('keys')
+print(person.keys())
+
+print('values')
+print(person.values())
